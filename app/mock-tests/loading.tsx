4599e23c08c38a6239d0d7fee29aa86893ea9ef9
purushothamkodidala02-crypto <purushothamkodidala02@gmail.com>
2026-08-28 18:08:21 +0530

    Update loading.tsx

--- app/mock-tests/loading.tsx
+++ app/mock-tests/loading.tsx
@@ -1,6 +1,3 @@
-import { PublicRouteLoading } from "@/components/feedback/RouteLoading";
-
 export default function Loading() {
-  return <PublicRouteLoading label="Loading mock tests" />;
+  return null;
 }
-
